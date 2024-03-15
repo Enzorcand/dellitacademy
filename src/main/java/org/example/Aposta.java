42697505a57e8527ede4481b678d6e043165eda4
Enzorcand <enzorcand@gmail.com>
2024-03-15 12:46:56 -0300

    Setup inicial projeto

---
+++ src/main/java/org/example/Aposta.java
@@ -0,0 +1,8 @@
+package org.example;
+
+import java.util.ArrayList;
+
+public class Aposta {
+    private ArrayList<Integer> numeros;
+    private boolean isVencedora;
+}
---
+++ src/main/java/org/example/Apostador.java
@@ -0,0 +1,9 @@
+package org.example;
+
+import java.util.ArrayList;
+
+public class Apostador {
+    private int cpf;
+    private String nome;
+    private ArrayList<Aposta> apostas;
+}
